7eae7a1c5defd1d8f6297fdf302f7883d87cc0bc
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-08-13 22:44:10 +0100

    Be generic over version of dotnet

--- nuget_push.sh
+++ nuget_push.sh
@@ -12,7 +12,7 @@ echo "version=$PACKAGE_VERSION" >> "$GITHUB_OUTPUT"
 
 tmp=$(mktemp)
 
-if ! dotnet nuget push "$SOURCE_NUPKG" --api-key "$NUGET_API_KEY" --source "$NUGET_SOURCE" > "$tmp" ; then
+if ! "$DOTNET_EXE" nuget push "$SOURCE_NUPKG" --api-key "$NUGET_API_KEY" --source "$NUGET_SOURCE" > "$tmp" ; then
     cat "$tmp"
     if grep 'already exists and cannot be modified' "$tmp" ; then
         echo "result=skipped" >> "$GITHUB_OUTPUT"
